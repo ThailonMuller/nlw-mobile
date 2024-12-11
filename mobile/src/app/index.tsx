0d223c42b96c6b10b3bb7a16277a09ef7d49804f
Thailon <thailon57@hotmail.com>
2024-12-10 21:46:03 -0300

    thirdcommit

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,13 +1,13 @@
-import {View, Text} from "react-native"
+import {View, Text} from "react-native";
+import { Welcome } from "@/components/welcome";
+import { Steps } from "@/components/steps";
 
 export default function Index(){
     return(
-        <View style={{flex: 1, 
-        justifyContent: "center", 
-        alignItems: "center"
-        }}>
+        <View style={{flex: 1, padding:40, gap:40 }}>
 
-          <Text style={{fontSize: 22}}> Hello React Native </Text>
+          <Welcome />
+          <Steps />
         </View>
     )
 }
